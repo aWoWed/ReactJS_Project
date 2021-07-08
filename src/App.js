15be154f57a426a28c476fcf925f040b86cfda27
Serhii Kovalov <andrey_roland@hotmail.com>
2021-07-09 00:43:02 +0300

    Lessons_1-11

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='app-wrapper'>
+        <header className='header'>
+            <img src='https://st4.depositphotos.com/9449108/25247/i/600/depositphotos_252470670-stock-photo-illustration-of-a-japanese-warrior.jpg'/>
+        </header>
+        <nav className='nav'>
+            <div>
+               <a>Profile</a>
+            </div>
+            <div>
+               <a>Messages</a>
+            </div>
+            <div>
+                <a>News</a>
+            </div>
+            <div>
+                <a>Music</a>
+            </div>
+            <div>
+                <a>Settings</a>
+            </div>
+        </nav>
+        <div className='content'>
+            <img src='https://wallpaperaccess.com/full/1198688.jpg'/>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
   );
 }
 
